6fb77309e36a6ef0da58f6bf509753aa6c75a664
matt <raffy4284@berkeley.edu>
2015-01-15 13:39:35 -0800

    Editing database for blogs

--- db/migrate/20150115202517_create_blogs.rb
+++ db/migrate/20150115202517_create_blogs.rb
@@ -1,6 +1,5 @@
 class CreateBlogs < ActiveRecord::Migration
   def change
-    add_index :title, unique: true, :author, :image, :year
     create_table :blogs do |t|
       t.string :title
       t.string :author
